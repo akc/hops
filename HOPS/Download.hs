6c19ba324956339b0bb06eba85c0c3a89890d798
Anders Claesson <akc@akc.is>
2015-09-19 15:14:32 +0100

    Rewrite download function as Network.HTTP.Conduit.withManager is deprecated

--- HOPS/Download.hs
+++ HOPS/Download.hs
@@ -4,18 +4,17 @@
 -- Maintainer  : Anders Claesson <anders.claesson@gmail.com>
 -- License     : BSD-3
 --
-module HOPS.Download (download, requestPage) where
+module HOPS.Download (download) where
 
 import Numeric
-import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as B
-import qualified Data.ByteString.Lazy.Char8 as BL
 import Data.Conduit hiding (($$))
 import Data.Conduit.Zlib (ungzip)
 import qualified Data.Conduit.Binary as CB
 import Network.HTTP.Conduit hiding (Proxy)
 import Network.HTTP.Types (hContentLength)
 import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.Resource (runResourceT)
 #if __GLASGOW_HASKELL__ < 710
 import Control.Applicative
 #endif
@@ -23,23 +22,17 @@ import HOPS.OEIS (URL)
 import System.Console.ANSI
 import System.IO
 
--- | Request a page at a given URL with specified key-value pairs (query
--- string).
-requestPage :: URL -> [(ByteString, ByteString)] -> IO ByteString
-requestPage url kvs = do
-    req <- setQueryString [(k, Just v) | (k,v) <- kvs] <$> parseUrl url
-    res <- withManager (httpLbs req)
-    return $ BL.toStrict (responseBody res)
-
 -- | Download a file at a given URL showing a progress indicator at the
 -- given column, and save it at a specified path.
 download :: Int -> URL -> FilePath -> IO ()
-download col url fpath = withManager $ \manager -> do
-    req <- parseUrl url
-    res <- http req manager
-    let Just cl = lookup hContentLength (responseHeaders res)
-    let n = read (B.unpack cl) :: Int
-    responseBody res $$+- progress n 0 =$ ungzip =$ CB.sinkFile fpath
+download col url fpath = do
+    req <- liftIO $ parseUrl url
+    man <- newManager tlsManagerSettings
+    runResourceT $ do
+        response <- http req man
+        let Just cl = lookup hContentLength (responseHeaders response)
+        let n = read (B.unpack cl) :: Int
+        responseBody response $$+- progress n 0 =$ ungzip =$ CB.sinkFile fpath
   where
     progress total acc = await >>= maybe (return ()) (\chunk -> do
         let acc' = acc + B.length chunk
