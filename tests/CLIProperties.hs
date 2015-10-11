7adef8eaabdf88bb6db02df679c010fd93dbb2fa
Anders Claesson <akc@akc.is>
2015-10-11 01:59:06 +0100

    Enable running CLI tests with cabal

---
+++ tests/CLIProperties.hs
@@ -0,0 +1,9 @@
+-- |
+-- Copyright   : Anders Claesson 2015
+-- Maintainer  : Anders Claesson <anders.claesson@gmail.com>
+-- License     : BSD-3
+--
+
+import System.Process
+
+main = callCommand "tests/cli-properties.sh"
